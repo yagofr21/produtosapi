fa1cec9a2a059fb5180eb3b648278939170bf500
Yago Freitas <yagofr21@gmail.com>
2026-07-01 20:53:51 -0300

    firts commit

---
+++ src/main/java/com/github/produtosapi/ProdutosapiApplication.java
@@ -0,0 +1,13 @@
+package com.github.produtosapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdutosapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdutosapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/produtosapi/ProdutosapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.produtosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutosapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
